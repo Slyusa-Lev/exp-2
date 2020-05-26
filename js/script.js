4faa61c4ade5aadea37b127d6d0c25bfe53c9cfe
Slyusa <dsg.slyusa@gmail.com>
2020-05-26 17:24:14 +0300

    v 1.0, not mask, not animations

--- js/script.js
+++ js/script.js
@@ -1,168 +1,192 @@
-const products = [
-  {
-      "id": 1,
-      "name": "Milk",
-      "cost": 120
-  },
-  {
-      "id": 2,
-      "name": "Potato",
-      "cost": 63
-  },
-  {
-      "id": 3,
-      "name": "Chocolate",
-      "cost": 270
-  },
-  {
-      "id": 4,
-      "name": "Coffee",
-      "cost": 167
-  },
-  {
-      "id": 5,
-      "name": "Cheese",
-      "cost": 399
-  },
-  {
-      "id": 6,
-      "name": "Sausage",
-      "cost": 320
+const nameMonths = ['январь', 'февраль', 'март', 'апрель', 'май', 'июнь', 'июль', 'август', 'сентябрь', 'октябрь', 'ноябрь', 'декабрь'];
+
+
+const form = document.forms.invest;
+const button = form.elements.submit;
+const outputInvestResult = document.querySelector('.output-invest-result');
+const table = document.createElement('table');
+const tableHeader = document.createElement('th');
+const tableRow = document.createElement('tr');
+const tableData = document.createElement('td');
+
+button.addEventListener("click", function(event) {
+  outputInvestResult.innerHTML = '';
+  event.preventDefault();
+	  
+  let investValue = {};
+  investValue.startSum = +form.elements.sum.value;
+	investValue.sumOfMonth = +form.elements.month.value;
+	investValue.procent = +form.elements.procent.value;
+	investValue.months = +form.elements.months.value;
+  
+  const nowDate = getNowDate();
+  const lastYear = getLastYear(nowDate, investValue.months);
+  
+  const investYears = getInvestYears(nowDate.nowYear, lastYear);
+  const investMonths = getInvestMonths(nowDate, investYears, investValue.months);
+  const investSumOfMonths = getInvestSumOfMonths(nowDate, investYears, investValue.months, investValue);
+  
+  console.log(investMonths);
+  console.log(investSumOfMonths);
+  
+  renderInvestTable(nowDate, investYears, investMonths, nameMonths, investSumOfMonths);
+  editInvestGraph(investValue, investSumOfMonths);
+});
+
+//новый код
+getNowDate = () => {
+	const nowDate = new Date();
+	const objDate = {
+		nowYear: nowDate.getFullYear(),
+		nowMonth: nowDate.getMonth()
+	}
+	return objDate;
+}
+
+rounded = (number) => {
+    return +number.toFixed(1);
+}
+
+getLastYear = (nowDate, numberOfMonth) => {
+  return Math.ceil(nowDate.nowYear + ((nowDate.nowMonth + numberOfMonth) / 12));
+}
+
+getInvestYears = (nowYear, lastYear) => {
+	const investYears = [];
+	for (let currentYear = nowYear; currentYear < lastYear; currentYear++) {
+		investYears.push(currentYear);
+    }
+	return investYears;
+}
+
+getInvestMonths = (nowDate, investYears, limitMonth) => {
+  let investMonths = [];
+  let monthsCounter = 1;
+  
+  for (let currentYear = 1; currentYear <= investYears.length; currentYear++) {
+    let startMonth = 0;
+    let monthsOfTheYear = [];
+    
+    if (monthsCounter === 1) {
+      startMonth = nowDate.nowMonth;
+    }
+    
+    for (let currentMonth = startMonth + 1; currentMonth < 12; currentMonth++) { //startMonth + 1
+      monthsOfTheYear.push(currentMonth);
+      monthsCounter++;
+      if (monthsCounter > limitMonth) {
+        break
+      }
+    }
+    
+    investMonths.push(monthsOfTheYear);
   }
-];
-
-function getById(products, id) {
-  var product = products.find(item => item.id == id);
-  //if (product == undefined) return 'Нет продукта с таким id';
-  return product;
+  
+  return investMonths;
 }
-/*
-function getById(products, id) {
-  for (var i = 0; i < products.length; i++) {
-    if (products[i].id == id) {
-        return products[i];
+
+getInvestSumOfMonths = (nowDate, investYears, limitMonth, investValue) => {
+  let monthsCounter = 1;
+  const procentOfMonth = investValue.procent / 100 / 12;
+  let accomulatorOfSum = 0;
+  let investSumOfMonths = [];
+  
+  for (let currentYear = 1; currentYear <= investYears.length; currentYear++) {
+    let startMonth = 0;
+    let monthsOfTheYear = [];
+    
+    if (monthsCounter === 1) {
+      startMonth = nowDate.nowMonth;
     }
+    
+    for (let currentMonth = startMonth + 1; currentMonth < 12; currentMonth++) { //startMonth + 1
+      if (monthsCounter === 1) {
+        accomulatorOfSum = Math.round(investValue.startSum + investValue.startSum * procentOfMonth);
+      } else {
+        const procentSumOfMonth = (accomulatorOfSum + investValue.sumOfMonth) * procentOfMonth;
+        accomulatorOfSum = Math.round(accomulatorOfSum + investValue.sumOfMonth + procentSumOfMonth);
+      }
+      monthsOfTheYear.push(accomulatorOfSum);
+      monthsCounter++;
+      if (monthsCounter > limitMonth) {
+        break
+      }
+    }
+    
+    investSumOfMonths.push(monthsOfTheYear);
   }
-
-*/
-
-function sortedDescending (products) {
-	function sortProductsDescending (a, b) {
-		return b.cost - a.cost;
-	}
-	
-	return products.sort(sortProductsDescending);
+  
+  return investSumOfMonths;
 }
 
-function sortedAscending (products) {
-	function sortProductsAscending (a, b) {
-		return a.cost - b.cost;
-	}
-
-	return products.sort(sortProductsAscending);
-}
-
-
-function renameById(products, id, newName) {
-  products.find(item => item.id == id).name = newName;
-	return products; 
-}
-/*function renameById(products, id, newName) {
-  for (var i = 0;i < products.length; i++ ) {
-    if (products[i].id == id) {
-      products[i].name = newName;
+renderInvestTable = (nowDate, investYears, investMonths, nameMonths, investSumOfMonths) => {
+  for (let i = 0; i < investYears.length; i++) {
+    let newTable = table.cloneNode(true);
+    newTable.id = 'table-' + investYears[i];
+    
+    let yearTableHeader = tableHeader.cloneNode(true);
+    yearTableHeader.textContent = investYears[i];
+    newTable.appendChild(yearTableHeader);
+    
+    let valueTableHeader = tableHeader.cloneNode(true);
+    valueTableHeader.textContent = investSumOfMonths[i][investSumOfMonths[i].length - 1];
+    newTable.appendChild(valueTableHeader);
+    
+    for (let j = 0; j <= investMonths[i][investMonths[i].length - 1]; j++) {
+      let newTableRow = tableRow.cloneNode(true);
+      
+      let monthTableData = tableData.cloneNode(true);
+      monthTableData.textContent = nameMonths[investMonths[i][j]]; //
+      newTableRow.appendChild(monthTableData);
+      
+      let valueTableData = tableData.cloneNode(true);
+      valueTableData.textContent = investSumOfMonths[i][j];
+      newTableRow.appendChild(valueTableData);
+      
+      newTable.appendChild(newTableRow);
     }
+    
+    outputInvestResult.appendChild(newTable);
   }
-	return products; 
-}*/
-
-function deleteById(products, id) {
-  var newProducts = products.filter(item => item.id !== id);
-  return newProducts;
 }
-//обратная логика! не нарезать массив, а собрать в новый массив все элементы id которых не совпадает с входящим значением
-/*function deleteById(products, id) { 
-  for (var i = 0; i < products.length; i++) { 
-    if (products[i].id == id) { 
-      var newProducts = products.slice(); 
-      newProducts.splice(i, 1); 
-      return newProducts; 
-    }
-  }
-}*/
 
-function searchAllParameters(products, search) {
-	var searchProducts = [];
-	searchProducts = products.filter( function(product) {
-    var productValue = Object.values(product);
-    if ( productValue.includes(search) ) return product;
-  });
-  return searchProducts;
+editInvestGraph = (investValue, investSumOfMonths) => {
+  const wrapperOutput = document.querySelector('.wrapper-output');
+  const finalSum = document.querySelector('#final-sum');
+  const investMonthsOutput = document.querySelector('#invest-months-output');
+  const sumsOfLastYear = investSumOfMonths[investSumOfMonths.length - 1];
+  const sumsOfLastMonth = sumsOfLastYear[sumsOfLastYear.length - 1];
+  
+  const allSumInvestGraph = document.querySelector('#all-sum-invest');
+  const procentsInvestGraph = document.querySelector('#procents-invest');
+  
+  const allSumInvestValueOutput = document.querySelector('#all-sum-invest-value');
+  const procentsInvestOutput = document.querySelector('#procents-invest-value');//эта строка
+  
+  const propertyStartSum = document.querySelector('#start-sum');
+  const propertySumOfMonth = document.querySelector('#sum-of-month');
+  const propertyProcent = document.querySelector('#procent');
+  const propertyMonths = document.querySelector('#months');
+  
+  const allSumInvestValue = investValue.startSum + (investValue.months - 1) * investValue.sumOfMonth;
+  const procentsInvestValue = sumsOfLastMonth - allSumInvestValue;
+  const procentsInvestProcents = rounded(procentsInvestValue / sumsOfLastMonth * 100);
+  const procentsAllSumInvest = 100 - procentsInvestProcents;
+  
+  wrapperOutput.style.display = "block";
+  finalSum.textContent = sumsOfLastMonth;
+  investMonthsOutput.textContent = investValue.months;
+  
+  allSumInvestGraph.title = `${procentsAllSumInvest}%, ${allSumInvestValue} руб.`;
+  allSumInvestGraph.style.width = `${procentsAllSumInvest}%`;
+  procentsInvestGraph.title = `${procentsInvestProcents}%, ${procentsInvestValue} руб.`;
+  procentsInvestGraph.style.width = `${procentsInvestProcents}%`;
+  
+  allSumInvestValueOutput.textContent = allSumInvestValue;
+  procentsInvestOutput.textContent = `${procentsAllSumInvest}%`;
+  
+  propertyStartSum.textContent = investValue.startSum;
+  propertySumOfMonth.textContent = investValue.sumOfMonth;
+  propertyProcent.textContent = investValue.procent;
+  propertyMonths.textContent = investValue.months;
 }
-/*function searchAllParameters(products, search) {
-	var searchProducts = [];
-	for (var i = 0; i < products.length; i++) {
-		for (var key in products[i]) {
-			if (products[i][key] == search) {
-				searchProducts.push(products[i]);
-			}
-		}
-	}
-	return searchProducts;
-}*/
-
-function getMaxCost(products) {
-  var productMaxCost = products.reduce(function (accumulator, currentValue) {
-	  if (currentValue.cost > accumulator.cost) {
-		  accumulator = currentValue;
-    }
-	  return accumulator;
-  }, products[0]);
-  return productMaxCost;
-}
-/*function getMaxCost(products) {
-	var productMaxCost;
-	var maxCost = 0;
-	
-	for (var i = 0; i < products.length; i++) {
-		if (maxCost < products[i].cost) {
-			maxCost = products[i].cost;
-			productMaxCost = products[i];
-		}
-	}
-	return productMaxCost;
-}*/
-
-function getMinCost(products) {
-	var productMinCostValue = products[0].cost;
-  var productMinCost = products.reduce(function (accumulator, currentValue) {
-	  if (currentValue.cost < productMinCostValue) {
-		  productMinCostValue = currentValue.cost;
-		  accumulator = currentValue;
-    }
-	  return accumulator;
-  }, products[0]);
-  return productMinCost;
-}
-/*function getMinCost(products) {
-	var productMinCost;
-	var minCost = products[0].cost;
-	
-	for (var i = 0; i < products.length; i++) {
-		if (minCost > products[i].cost) {
-			minCost = products[i].cost;
-			productMinCost = products[i];
-		}
-	}
-	return productMinCost;
-}*/
-
-
-console.log( getById(products, 2) );
-console.log( sortedDescending(products) ); //нужно переделать, чтобы объект клонировался
-console.log( sortedAscending(products) ); //нужно переделать, чтобы объект клонировался
-console.log( renameById(products, 4, 'noCoffee') );
-console.log( deleteById(products, 4) );
-console.log( searchAllParameters(products, 'Cheese') );
-console.log( getMaxCost(products) );
-console.log( getMinCost(products) );
